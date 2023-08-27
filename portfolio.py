6af16b39ce683a5019b36d282892cbce7b93d35a
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-08-27 18:02:46 +0530

    added about me above columns cropped photo

--- portfolio.py
+++ portfolio.py
@@ -6,6 +6,7 @@ col1, col2 = st.columns(2)
 with col1:
     st.image("images/my photo.jpeg")
 
+
 with col2:
     st.title("Navyam Raushan")
     content = """I am Navyam Raushan, a second year engineering student currently pursuing BTech in Computer Science and 
@@ -13,5 +14,10 @@ with col2:
             become a ML engineer, I am always eager to learn new things and grow with the Technology."""
 
     st.info(content)
+
+aboutme = """<h4>I am a Python Developer, here are some 
+              of my apps. Feel free to contact me.</h4>"""
+st.write(aboutme, unsafe_allow_html=True)
+
 
 
