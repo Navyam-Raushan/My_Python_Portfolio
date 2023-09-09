4fa889bb788168cb6256a1dfaa59f5518da7afa6
Navyam-Raushan <125335352+Navyam-Raushan@users.noreply.github.com>
2023-09-09 14:49:41 +0530

    Day 23: Send mails using form

--- pages/Contact_Us.py
+++ pages/Contact_Us.py
@@ -1,12 +1,26 @@
 import streamlit as st
+from send_email import send_mail
 
 st.title("Contact Us")
 
-with st.form(key="my_form"):
-    mail_id = st.text_input("Enter your email I'D")
-    message = st.text_area("Enter your message")
+with st.form(key="email_form"):
+    sender_mail_id = st.text_input("Enter your email id")
+    raw_message = st.text_area("Enter your message")
+
+    # We need formatted message to see all details
+    # we must add a break line after subject and "\"
+
+    message = f"""\
+Subject: New email from {sender_mail_id}
+
+From: {sender_mail_id}
+{raw_message}
+
+"""
+    # message = message + "\n" + sender_mail_id
     button = st.form_submit_button("Submit")
 
     # The button will only be executed if it's pressed as it takes true.
     if button:
-        print("I am pressed.")
+        send_mail(message)
+        st.info("Your Email send Successfully.")
--- send_email.py
+++ send_email.py
@@ -1,28 +1,23 @@
 import smtplib
 import ssl
 
-# This is for the connection.
-host = "smtp.gmail.com"
-port = 465
 
-# This password is created by google and only used by this app.
-username = "bsarthak935@gmail.com"
-password = "lupwevndnduujnys"
+# creating function by refactor of pycharm
+def send_mail(message):
+    # This is for the connection.
+    host = "smtp.gmail.com"
+    port = 465
+    # This password is created by google and only used by this app.
+    username = "bsarthak935@gmail.com"
+    password = "lupwevndnduujnys"
 
-receiver = "navyamraushan@gmail.com"
-# Context means to send mail securely *using ssl.
-context = ssl.create_default_context()
+    # I am receiver in this case and this app will login me first.
+    receiver = "bsarthak935@gmail.com"
+    # Context means to send mail securely *using ssl.
+    context = ssl.create_default_context()
 
-# First line will act like subject (don't forget \)
-message = """\
-Subject: First automated Mail
-Hi
-Its first automated mail from python.
-check it out.
-"""
-
-# order does matter while providing parameters down see implementation.
-with smtplib.SMTP_SSL(host, port, context=context) as server:
-    server.login(username, password)
-    #     to send emails
-    server.sendmail(username, receiver, message)
+    # order does matter while providing parameters down see implementation.
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        #     to send emails
+        server.sendmail(username, receiver, message)
